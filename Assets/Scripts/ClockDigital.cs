ea4d0f552de4143f3c922b59ba8ac5a1ee3c90f3
Scott Bromander <scott@primeacademy.io>
2016-06-21 15:46:41 -0500

    initial commit

---
+++ Assets/Scripts/ClockDigital.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+using System;
+
+public class ClockDigital : MonoBehaviour {
+
+	private Text textClock;
+
+	// Use this for initialization
+	void Start () {
+		textClock = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DateTime time = DateTime.Now;
+		string hour = LeadingZero (time.Hour);
+		string minute = LeadingZero (time.Minute);
+		string second = LeadingZero (time.Second);
+
+		textClock.text = hour + ':' + minute + ':' + second;
+	}
+
+	string LeadingZero(int n){
+		return n.ToString ().PadLeft (2, '0');
+	}	
+}
---
+++ Assets/Scripts/DigitalCountdown.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class DigitalCountdown : MonoBehaviour {
+	private Text textClock;
+
+	private float countdownTimerDuration;
+	private float countdownTimerStartTime;
+
+	// Use this for initialization
+	void Start () {
+		textClock = GetComponent<Text>();
+
+		CountdownTimerReset (30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Default, saying 'we done', unless that is not the case.
+		string timerMessage = "Countdown has finished";
+
+		//I assume this is some sort of 'casting' of the return? Like type conversion?
+		int timeLeft = (int)CountdownTimerSecondsRemaining();
+
+		//Basic conditional
+		if (timeLeft > 0) {
+			//If there is time left, reset the timerMessage to read what time is left,
+			//Processing the TimeLeft with the Leading Zero function to get 'nice' looking information.
+			timerMessage = "Countdown Seconds Remaining: " + LeadingZero (timeLeft);
+		}
+
+		//Update the actual text of the GUI element to reflect the message from the logic.
+		textClock.text = timerMessage;
+	}
+
+	private void CountdownTimerReset (float delayInSeconds) {
+		//Spin up a new timer, then capture the current system time so we can track the change in time.
+		countdownTimerDuration = delayInSeconds;
+		countdownTimerStartTime = Time.time;
+	}
+
+	private float CountdownTimerSecondsRemaining () {
+		//Grab the current system time, and find the difference between that and when we started ("StartTime")
+		float elapsedSeconds = Time.time - countdownTimerStartTime;
+		//Check the difference between the Time Duration and the observed Delta noted above
+		float timeLeft = countdownTimerDuration - elapsedSeconds;
+		return timeLeft;
+	}
+
+	private string LeadingZero(int n){
+		return n.ToString ().PadLeft (2, '0');
+	}
+}
